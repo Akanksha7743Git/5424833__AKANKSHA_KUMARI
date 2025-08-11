5125a04d813095f8a1e2c5519483c9f278a60ce6
Akanksha7743Git <akankshakumaricse5@gmail.com>
2025-08-11 10:46:30 +0530

    new upload

---
+++ HackerRank/Week-1/Lonely Integer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int count=0;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        count^=arr[i];
+        
+    }
+    cout<<count;
+    
+    return 0;
+}    
